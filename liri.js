6a57eb1734cae3d6d7088de69575032163e5b50c
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-04-17 20:25:39 -0500

    itital twitter and spotify works

--- liri.js
+++ liri.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 
-var spotifyKey = keys.spotify;
-var clientKey = keys.twitter;
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+
+var userCommand = process.argv[2];
+
+
 
 console.log(spotify)
 console.log(client)
