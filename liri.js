8c9ff5e30cf2f8aa80e356ccf4ac0c7f9fc305c4
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-04-20 21:32:46 -0500

    spotify return is sorted and working

--- liri.js
+++ liri.js
@@ -10,17 +10,20 @@ var client = new Twitter(keys.twitter);
 
 var userCommand = process.argv[2];
 
-// client.get('status/user_timeline', function(error, tweets, response) {
-// 	if (!error) {
-// 		console.log(response);
-// 	}
+var params = {screen_name: 'jordan_bootcamp'}
+
+// client.get('status/user_timeline', params, function(error, tweets, response) {
+	
+// 		console.log(tweets);
+
 // })
 
-spotify
-  .search({ type: 'track', query: 'All the Small Things' })
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+spotify.search({ type: 'track', query: 'Bloody Sunday', limit: '1'}, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data.tracks.items[0].artists[0].name);
+console.log(data.tracks.items[0].name);
+console.log(data.tracks.items[0].external_urls.spotify) 
+});
