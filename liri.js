6b446ee7cee95d2dd7570988aaa534725d190ff1
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-04-21 05:56:18 -0500

    omdb request working

--- liri.js
+++ liri.js
@@ -30,14 +30,30 @@ for (var i = 2; i < userArgs.length; i++) {
 
 // })
 
-spotify.search({ type: 'track', query: userQuery, limit: '1'}, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
+// spotify.search({ type: 'track', query: userQuery, limit: '1'}, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
  
-console.log(data.tracks.items[0].artists[0].name);
-console.log(data.tracks.items[0].name);
-console.log(data.tracks.items[0].external_urls.spotify) 
+// console.log(data.tracks.items[0].artists[0].name);
+// console.log(data.tracks.items[0].name);
+// console.log(data.tracks.items[0].external_urls.spotify) 
+// });
+
+var queryUrl = "http://www.omdbapi.com/?t=" + userQuery + "&y=&plot=short&apikey=trilogy";
+
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  if (!error && response.statusCode === 200) {
+	console.log("Title: " + JSON.parse(body).Title);
+	console.log("Release Year: " + JSON.parse(body).Year);
+	console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+	console.log("Rotten Tomatoes Score: " + JSON.parse(body).Ratings[1].Value);
+	console.log("Country Produced: " + JSON.parse(body).Country);
+	console.log("Language: " + JSON.parse(body).Language);
+	console.log("Plot: " + JSON.parse(body).Plot);
+	console.log("Actors: " + JSON.parse(body).Actors);
+  }
 });
-
-
