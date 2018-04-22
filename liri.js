95b73df6248c8c4cd120d0fef34cd98be30ae308
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-04-22 18:41:27 -0500

    twiiter finally works

--- liri.js
+++ liri.js
@@ -22,38 +22,39 @@ for (var i = 2; i < userArgs.length; i++) {
 	}
 }
 
-// var params = {id: 985662413080055809}
 
-// client.get('status/user_timeline', params, function(error, tweets, response) {
+var params = {id:'jskophammer'}
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
 	
-// 		console.log(tweets);
+		console.log(tweets);
 
-// })
+})
 
 // spotify.search({ type: 'track', query: userQuery, limit: '1'}, function(err, data) {
 //   if (err) {
 //     return console.log('Error occurred: ' + err);
 //   }
  
-// console.log(data.tracks.items[0].artists[0].name);
-// console.log(data.tracks.items[0].name);
-// console.log(data.tracks.items[0].external_urls.spotify) 
+// console.log("Artist: " + data.tracks.items[0].artists[0].name);
+// console.log("Album: " + data.tracks.items[0].name);
+// console.log("Song Link: " + data.tracks.items[0].external_urls.spotify) 
 // });
 
-var queryUrl = "http://www.omdbapi.com/?t=" + userQuery + "&y=&plot=short&apikey=trilogy";
+// var queryUrl = "http://www.omdbapi.com/?t=" + userQuery + "&y=&plot=short&apikey=trilogy";
 
-console.log(queryUrl);
+// console.log(queryUrl);
 
-request(queryUrl, function(error, response, body) {
+// request(queryUrl, function(error, response, body) {
 
-  if (!error && response.statusCode === 200) {
-	console.log("Title: " + JSON.parse(body).Title);
-	console.log("Release Year: " + JSON.parse(body).Year);
-	console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-	console.log("Rotten Tomatoes Score: " + JSON.parse(body).Ratings[1].Value);
-	console.log("Country Produced: " + JSON.parse(body).Country);
-	console.log("Language: " + JSON.parse(body).Language);
-	console.log("Plot: " + JSON.parse(body).Plot);
-	console.log("Actors: " + JSON.parse(body).Actors);
-  }
-});
+//   if (!error && response.statusCode === 200) {
+// 	console.log("Title: " + JSON.parse(body).Title);
+// 	console.log("Release Year: " + JSON.parse(body).Year);
+// 	console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+// 	console.log("Rotten Tomatoes Score: " + JSON.parse(body).Ratings[1].Value);
+// 	console.log("Country Produced: " + JSON.parse(body).Country);
+// 	console.log("Language: " + JSON.parse(body).Language);
+// 	console.log("Plot: " + JSON.parse(body).Plot);
+// 	console.log("Actors: " + JSON.parse(body).Actors);
+//   }
+// });
